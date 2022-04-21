164619bfcc3bccaee9da87af11c576aa2d4bf6bb
Monkeman9592 <98450342+Monkeman9592@users.noreply.github.com>
2022-04-20 17:32:05 -0700

    boom it works
    it finally works

--- termiunalchecklist.py
+++ termiunalchecklist.py
@@ -1,2 +1,68 @@
-mst = "wackysluch"
-print(mst)
+import random
+
+while True:
+    user_action = input("Enter a choice (rock, paper, scissors): ")
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
+
+    checklist = 
+
+# Define Functions
+def create(item):
+    # Create item code here
+
+def read(index):
+    # Read code here
+
+def update(index, item):
+    # Update code here
+
+def destroy(index):
+    # Destroy code here
+
+def mark_completed(index):
+    # Add code here that marks an item as completed
+
+def list_all_items():
+    # List all items code here
+
+def user_input(prompt):
+    # Get user input here
+
+def select(function_code):
+    # User Selection Code here
+
+    # Create item example
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+running = True
+
+while running:
+    selection = user_input(
+        "Press C to add to list, R to Read from list, P to display list, and Q to quit")
+    running = select(selection)
