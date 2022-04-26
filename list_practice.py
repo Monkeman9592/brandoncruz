c75a825e4f2fb17d605c254f3451f9f7fc2373f7
Monkeman9592 <98450342+Monkeman9592@users.noreply.github.com>
2022-04-26 00:15:47 -0700

    list update
    added remaining functions

--- list_practice.py
+++ list_practice.py
@@ -1,4 +1,4 @@
-Us_cities=["oakland'" "New york", "hollywood","san jose","san deago","berkly", "eufala", "fresno", "san carlos", "portland", "chicago"]
+Us_cities=["oakland'" "New york", "hollywood","san jose","san diago","berkly", "eufala", "fresno", "san carlos", "portland", "chicago"]
 print(Us_cities)
  
 print(Us_cities[1], Us_cities[3],Us_cities[4])
@@ -25,3 +25,23 @@ bestcuntrys = cuntrys[3:7]
 print(bestcities)
 print(bestcuntrys)
 
+Us_cities [1] = "oakland"
+Us_cities [0] = "WYOMING"
+Us_cities [0] = "miami"
+Us_cities [2] = "reno"
+Us_cities [7] = "pitsburg"
+Us_cities [5] = "moscow"
+
+print(Us_cities)
+
+Us_cities.append ("oakland")
+Us_cities.extend(["hollywood","san diago"])
+Us_cities.insert(0, "miami")
+
+print(Us_cities)
+
+del Us_cities[4]
+Us_cities.pop(2)
+
+Us_cities.remove("oakland")
+print(Us_cities)
