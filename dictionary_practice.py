0582ed353650ff76756b5f85b96e6ba9023208f9
Monkeman9592 <98450342+Monkeman9592@users.noreply.github.com>
2022-04-26 00:06:47 -0700

    list_dictionary_operators
    all asignments

---
+++ dictionary_practice.py
@@ -0,0 +1,58 @@
+d1 = {"water" : 10, "coke" : 10, "arizona" : 10}
+ 
+d2 = {"mango" : 10, "banana" : 10, "pineapple" : 10}
+ 
+print(d1, d2)
+ 
+ 
+#set a new variable to d1 and d2
+banana_bowl = d2 ["banana"]
+ice_water = d1 ["water"]
+coke_icy = d1["coke"]
+arizona_tea =d1 ["arizona"]
+mango_smothy = d2["mango"]
+pinea_calada = d2["pineapple"]
+ 
+#print(banana_bowl ,ice_water, coke_icy, arizona_tea, mango_smothy, pinea_calada)
+ 
+ 
+d1["banana"] = 3
+#print(d1)
+ 
+shoes = {"jordan" : 1, "yeezy" : 8, "foamposite" : 10, "air max" : 5, "SB dunk" : 20, }
+ 
+shoes ["SB dunk"] -= 2
+ 
+shoes ["yeezy"] += 1
+ 
+shoes["jordan"] += 7
+shoes["yeezy"] +=7
+shoes["foamposite"] +=7
+shoes["air max"] +=7
+shoes["SB dunk"] +=7
+ 
+print(shoes)
+ 
+shoes["jordan"] -= 3
+shoes["yeezy"] -=3
+shoes["foamposite"] -=3
+shoes["air max"] -=3
+shoes["SB dunk"] -=3
+ 
+print(shoes)
+ 
+del shoes["yeezy"]
+print(shoes)
+ 
+shoes["nike"] = '10'
+shoes["adidas"] = '10'
+shoes["asics"] = '10'
+print(shoes)
+ 
+ 
+shoes["air max"] -=3
+shoes["SB dunk"] -=3
+ 
+print(shoes)
+ 
+
---
+++ list_practice.py
@@ -0,0 +1,27 @@
+Us_cities=["oakland'" "New york", "hollywood","san jose","san deago","berkly", "eufala", "fresno", "san carlos", "portland", "chicago"]
+print(Us_cities)
+ 
+print(Us_cities[1], Us_cities[3],Us_cities[4])
+three_cities = Us_cities[0 : 3]
+print(three_cities)
+ 
+ 
+ 
+cuntrys = ["usa", "mexico", "china", "russia", "australia", "south america", ]
+
+print(cuntrys[0])
+
+print(Us_cities[2])
+print(Us_cities[4])
+print(Us_cities[5])
+
+print(Us_cities[1])
+print(Us_cities[3])
+print(Us_cities[5])
+
+bestcities = Us_cities[0:3]
+bestcuntrys = cuntrys[3:7]
+
+print(bestcities)
+print(bestcuntrys)
+
