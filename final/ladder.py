969e5678823f975eaee495330e1287cae7e6d766
Monkeman9592 <98450342+Monkeman9592@users.noreply.github.com>
2022-11-03 16:17:59 -0700

    bNBnn
    hnmnmmm

---
+++ final/ladder.py
@@ -0,0 +1,14 @@
+b = int(input("Please input choice: of bananas 1-4 "))
+if b == 1:
+    print("you have 1 banana")
+else:
+    if b == 2:
+        print("you have 2 bananas")
+    else:
+        if b == 3:
+            print("you have 3 bananas")
+        else:
+            if b == 4:
+                print("thats a lot of bananas you got there ")  
+            elif 4 < b:
+                print("hey man thats to many bananas give me your legs")
---
+++ final/mush.py
@@ -0,0 +1,15 @@
+def multi_2(x):
+    
+    print("your age x 2 = ", x * 2)
+    
+def multi_3(x):
+    
+    print("your age x 3 = ", x / 3)
+    
+
+def main():
+    x = int(input("Enter you age: "))
+    multi_2(x)
+    multi_3(x)
+
+main()
