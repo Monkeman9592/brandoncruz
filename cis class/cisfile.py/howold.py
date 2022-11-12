61a32c81bd3f81ee99497d339b3c2ad791076d26
Monkeman9592 <98450342+Monkeman9592@users.noreply.github.com>
2022-11-12 15:10:35 -0800

    how old if and elif
    added if else to set a range of ages

--- cis class/cisfile.py/howold.py
+++ cis class/cisfile.py/howold.py
@@ -1,4 +1,9 @@
 name = input("Please enter your name: ")
 age = int(input("how old are you?"))
 
-print(name + "and will be" , age + 5 , " in 10 years")
+if 0 <= age <=  100:
+    print(name + " and will be" , age + 5 , " in 10 years")
+elif age <= 0:
+    print("People cannot have a negative age")
+elif age >= 100:
+    print("100 is the maximum age for this program")
